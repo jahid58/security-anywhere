1e2aee27344596e55bf84e461eeaaddf813a43b1
Jahid Hasan <jahidjjaa@gmail.com>
2021-05-07 13:40:42 +0600

    Delete firebaseConfig.js

--- src/components/Login/Login/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
- const firebaseConfig = {
-    apiKey: "AIzaSyAM6a6uLLPWe-0zRcyhTI6fKMrAFzVKmWU",
-    authDomain: "security-anywhere.firebaseapp.com",
-    projectId: "security-anywhere",
-    storageBucket: "security-anywhere.appspot.com",
-    messagingSenderId: "640195515956",
-    appId: "1:640195515956:web:49fba0bcc5e1ec93f85c61"
-  };
-  export default firebaseConfig
